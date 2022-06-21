de4080173a5381a4d7a19b6378c0ed888c7d8371
sivizdral <ivancvetic@outlook.com>
2022-06-21 14:10:07 +0200

    Finished warehouse app.

--- iepProjekat/applications/warehouse/warehouseApp.py
+++ iepProjekat/applications/warehouse/warehouseApp.py
@@ -1,54 +1,56 @@
-from flask import Flask;
-from applications.configuration import Configuration;
-from applications.models import database, Thread;
-from redis import Redis;
-from adminDecorator import roleCheck;
-from flask_jwt_extended import JWTManager;
+from flask import Flask, request, Response, jsonify
+from applications.configuration import Configuration
+from applications.models import *
+from redis import Redis
+from warehousemanDecorator import isWarehouseman
+from flask_jwt_extended import JWTManager
+import json, csv
 
-application = Flask ( __name__ );
-application.config.from_object ( Configuration )
-jwt = JWTManager ( application );
-
-@application.route ( "/threads", methods = ["GET"] )
-@roleCheck ( role = "admin" )
-def getThreadsPendingApproval ( ):
-    with Redis ( host = Configuration.REDIS_HOST ) as redis:
-        bytesList = redis.lrange ( Configuration.REDIS_THREADS_LIST, 0, 0 );
-        if ( len ( bytesList ) != 0 ):
-            title = bytesList[0].decode ( "utf-8" );
-            return title;
-        else:
-            return "No threads pending approval";
-
-@application.route ( "/threads/approve", methods = ["GET"] )
-def approveThreadProposal ( ):
-    with Redis ( host = Configuration.REDIS_HOST ) as redis:
-        bytes = redis.lpop ( Configuration.REDIS_THREADS_LIST );
-        title = bytes.decode ( "utf-8" );
-        thread = Thread ( title = title );
-        database.session.add ( thread );
-        database.session.commit ( );
-
-        bytesList = redis.lrange ( Configuration.REDIS_THREADS_LIST, 0, 0 );
-        if ( len ( bytesList ) != 0 ):
-            title = bytesList[0].decode ( "utf-8" );
-            return title;
-        else:
-            return "No threads pending approval";
-
-@application.route ( "/threads/decline", methods = ["GET"] )
-def declineThreadProposal ( ):
-    with Redis ( host = Configuration.REDIS_HOST ) as redis:
-        redis.lpop ( Configuration.REDIS_THREADS_LIST );
-
-        bytesList = redis.lrange ( Configuration.REDIS_THREADS_LIST, 0, 0 );
-        if ( len ( bytesList ) != 0 ):
-            title = bytesList[0].decode ( "utf-8" );
-            return title;
-        else:
-            return "No threads pending approval";
+application = Flask(__name__)
+application.config.from_object(Configuration)
+jwt = JWTManager(application)
 
 
-if (__name__ == "__main__"):
-    database.init_app ( application );
-    application.run ( debug = True, port = 5001 );
+@application.route("/update", methods=["POST"])
+@isWarehouseman(role="warehouseman")
+def updateProducts():
+    fileName = request.json.get("file", "")
+
+    if len(fileName) == 0:
+        return Response(json.dumps({"message":"Field file missing."}), status=400)
+
+    csv_file = open(fileName)
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    redis = Redis(host=Configuration.REDIS_HOST)
+
+    for row in csv_reader:
+        if len(row) != 4:
+            msg = "Incorrect number of values on line " + str(line_count) + "."
+            return Response(json.dumps({"message":msg}), status=400)
+
+        if not row[2].isdigit() or int(row[2]) == 0:
+            msg = "Incorrect quantity on line " + str(line_count) + "."
+            return Response(json.dumps({"message":msg}), status=400)
+
+        try:
+            price = float(row[3])
+        except ValueError:
+            msg = "Incorrect price on line " + str(line_count) + "."
+            return Response(json.dumps({"message":msg}), status=400)
+
+        if price == 0:
+            msg = "Incorrect price on line " + str(line_count) + "."
+            return Response(json.dumps({"message": msg}), status=400)
+
+        line_count += 1
+        sendMessage = ','.join(row)
+        redis.publish(channel="warehouseUpdates", message=sendMessage)
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    db.init_app(application)
+    application.run(debug=True, port=5001)
--- iepProjekat/applications/warehouse/warehousemanDecorator.py
+++ iepProjekat/applications/warehouse/warehousemanDecorator.py
@@ -1,14 +1,22 @@
-def roleCheck ( role ):
-    def innerRole ( function ):
-        @wraps ( function )
-        def decorator ( *arguments, **keywordArguments ):
-            verify_jwt_in_request ( );
-            claims = get_jwt ( );
-            if ( ( "roles" in claims ) and ( role in claims["roles"] ) ):
-                return function ( *arguments, **keywordArguments );
+from functools import wraps
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, create_refresh_token, get_jwt, get_jwt_identity, verify_jwt_in_request
+from flask import jsonify
+
+
+def isWarehouseman(role):
+    def warehousemanDecorator(func):
+        @wraps(func)
+        def decorator(*arguments, **keywordArguments):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if "roles" in claims:
+                if role in claims["roles"]:
+                    return func(*arguments, **keywordArguments)
+                else:
+                    ret = {"msg": "Missing Authorization Header"}
+                    return jsonify(ret), 401
             else:
-                return Response ( "permission denied!", status = 403 );
-
-        return decorator;
-
-    return innerRole;
+                ret = {"msg": "Missing Authorization Header"}
+                return jsonify(ret), 401
+        return decorator
+    return warehousemanDecorator
